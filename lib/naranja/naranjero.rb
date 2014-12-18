795b6ef90707d0921672b25ffaac4a5ff3498b04
alu0100315462 <alu0100315462@ull.edu.es>
2014-12-18 23:19:45 +0000

    Añadidas las expectativas y los metodos vacios que las satisfaran

--- lib/naranja/naranjero.rb
+++ lib/naranja/naranjero.rb
@@ -4,12 +4,33 @@ module Naranja
   #clase que define una respuesta
   class Naranjero
     
+    def initialize
+
+    end
+    
+    def altura
+
+    end
+    
+    def edad
+
+    end
+    
+    def numeroNaranjas
+
+    end
+
+    def uno_mas
+
+    end
+  
+    def recolectar_una
+
+    end
+
+    def run
+
+    end
   end
 
 end
-
-
-
-
-
-
--- spec/naranjero_spec.rb
+++ spec/naranjero_spec.rb
@@ -4,11 +4,35 @@ require './lib/naranjero.rb'
 
 describe Naranja::Naranjero do
     before :all do
-
+        @naranjero = Naranjero.new
     end
     
-    it "" do
-      
+    it "La clase debe tener un metodo que inicialice las variables necesarias" do
+      expect(@naranjero).to respond_to(:initialize)
+    end
+    
+    it "La clase debe tener un metodo que devuelva la altura actual del naranjero" do
+      expect(@naranjero).to respond_to(:altura)
+    end
+    
+    it "La clase debe tener un metodo que devuelva la edad actual del naranjero" do
+      expect(@naranjero).to respond_to(:edad)
+    end
+    
+    it "La clase debe tener un metodo que devuelva el numero de naranjas producidas este año" do
+      expect(@naranjero).to respond_to(:numeroNaranjas)
+    end
+    
+    it "La clase debe tener un metodo que aumente la edad del naranjero un año y todo lo que ello conlleva" do
+      expect(@naranjero).to respond_to(:uno_mas)
+    end
+    
+    it "La clase debe tener un metodo que haga que el recolector intente recoger una naranja" do
+      expect(@naranjero).to respond_to(:recolectar_una)
+    end
+    
+    it "La clase debe tener un metodo que cree los threads que representan el crecimiento y la recoleccion" do
+      expect(@naranjero).to respond_to(:run)
     end
 end
 
